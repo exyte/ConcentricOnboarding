4396965ba9a8225f44c9309c91752a9524f80945
baegteun <baegteun@gmail.com>
2022-04-07 10:41:18 +0900

    :rocket: :: Platform(ios 14+), swiftVersion(5+)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ConcentricOnboarding",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -25,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "ConcentricOnboardingTests",
             dependencies: ["ConcentricOnboarding"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
