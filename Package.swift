71e04a55323a79d05c9f5361f57778af443b3d35
AlisaMylnikova <alisa.mylnikova@exyte.com>
2023-04-20 15:48:57 +0700

    Update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,21 +9,15 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConcentricOnboarding",
             targets: ["ConcentricOnboarding"]),
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConcentricOnboarding",
-            dependencies: []),
-        .testTarget(
-            name: "ConcentricOnboardingTests",
-            dependencies: ["ConcentricOnboarding"]),
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: []
+        )
+    ]
 )
