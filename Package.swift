0f2c63d8a00f30e016481e99a394c34f08cb7262
AlisaMylnikova <alisa.mylnikova@exyte.com>
2023-04-20 15:44:07 +0700

    Update package

--- Package.swift
+++ Package.swift
@@ -14,17 +14,13 @@ let package = Package(
             name: "ConcentricOnboarding",
             targets: ["ConcentricOnboarding"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConcentricOnboarding",
-            dependencies: [],
-            path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "ConcentricOnboardingTests",
             dependencies: ["ConcentricOnboarding"]),
